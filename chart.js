299a24fbc6f1ebf4f8df1a4b39dde2a83b05570f
gust9196 <gust9196@stud.kea.dk>
2022-10-12 11:25:20 +0200

    piechart

--- chart.js
+++ chart.js
@@ -45,3 +45,32 @@ const barChart = new Chart(ctx1, {
     }
 });
 
+const ctx2 = document.querySelector('#chart2').getContext('2d');
+ctx2.canvas.width = 200;
+ctx2.canvas.height = 50;
+const chart2 = new Chart(ctx2, {
+    type: 'pie',
+    data: {
+        labels: ['Approach', 'Landing Roll', 'Take-off Run', 'Climb', 'En Route', 'Departure', 'Decent', 'Local', 'Arrival', 'Taxi', 'Parked',],
+        datasets: [{
+            data: [17543, 7617, 6097, 5145, 1509, 1466, 567, 493, 358, 171, 25],
+            label: "Avg cost for repairs",
+            fill: false,
+            tension: 0.1,
+            backgroundColor: [
+                'rgba(255, 99, 132)',
+                'rgba(255, 159, 64)',
+                'rgba(255, 205, 86)',
+                'rgba(75, 192, 192)',
+                'rgba(54, 162, 235)',
+                'rgba(153, 102, 255)',
+                'rgba(201, 203, 207)',
+                'rgba(201, 290, 210)',
+                'rgba(120, 40, 210)',
+            ],        },
+
+        ]
+
+    }
+});
+
